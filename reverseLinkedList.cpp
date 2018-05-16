e22f347d321bd9016094ba1b59a64bfc8583089f
fengjiran <fengjiran@foxmail.com>
2018-05-16 16:32:47 +0800

    completed

--- reverseLinkedList.cpp
+++ reverseLinkedList.cpp
@@ -7,9 +7,22 @@ struct ListNode
     struct ListNode *next;
 };
 
+// 从第二个结点开始，依次将结点插入第一个结点之前，完成翻转
 struct ListNode *reverseList(struct ListNode *head)
 {
     if (head == NULL)
         return NULL;
-    //
+
+    struct ListNode *pre = head;
+    struct ListNode *cur = head->next;
+
+    while (cur != NULL)
+    {
+        pre->next = cur->next;
+        cur->next = head;
+        head = cur;
+        cur = pre->next;
+    }
+
+    return head;
 }
