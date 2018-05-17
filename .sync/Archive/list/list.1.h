94a3d548907ff9c92f9311e87aca3e080c25aabe
fengjiran <fengjiran@foxmail.com>
2018-05-17 17:07:13 +0800

    add

--- .sync/Archive/list/list.1.h
+++
@@ -1,4 +0,0 @@
-#ifndef _LIST_H_
-#define _LIST_H_
-
-#endif // _LIST_H_
--- .sync/Archive/list/list_operation.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "list.h"
-
-Status initListHead(LinkList L) //构造一个带头结点的空单链表
-{
-    //
-}
