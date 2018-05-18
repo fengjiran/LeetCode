f80d06d3a6bb515e55aa75933553ae9dda2329bc
fengjiran <fengjiran@foxmail.com>
2018-05-18 23:35:25 +0800

    add head file

--- stack.h
+++ stack.h
@@ -1,4 +1,16 @@
 #ifndef _STACK_H_
 #define _STACK_H_
 
+#define STACK_INIT_SIZE 100
+#define STACKINCREMENT 10
+
+typedef int ElemType;
+
+typedef struct
+{
+    ElemType *base;
+    ElemType *top;
+    int stacksize;
+} SqStack;
+
 #endif
