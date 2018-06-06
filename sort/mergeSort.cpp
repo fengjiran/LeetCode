fb33147186497a9a66bdd4f3b7c0333bda8ab779
fengjiran <fengjiran@foxmail.com>
2018-06-06 21:26:28 +0800

    merge function

--- sort/mergeSort.cpp
+++ sort/mergeSort.cpp
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//合并两个数组，不使用哨兵
+//合并两个有序数组，不使用哨兵
 void merge(int *data, int nLpos, int nRpos, int nRightEnd)
 {
     int i;
+    int k = nLpos;
+    int nLeftEnd = nRpos;
+    int nTmpPos = 0;
+    int nLen = nRightEnd - nLpos + 1;
+
+    int *a = (int *)malloc(sizeof(int) * nLen);
+
+    nRpos++;
+
+    while (nLpos <= nLeftEnd && nRpos <= nRightEnd)
+    {
+        if (data[nLpos] <= data[nRpos])
+            a[nTmpPos++] = data[nLpos++];
+        else
+            a[nTmpPos++] = data[nRpos++];
+    }
+
+    while (nLpos <= nLeftEnd)
+        a[nTmpPos++] = data[nLpos++];
+
+    while (nRpos <= nRightEnd)
+        a[nTmpPos++] = data[nRpos++];
+
+    nTmpPos = 0;
+
+    for (i = k; i < nLen; i++)
+        data[i] = a[nTmpPos++];
+
+    free(a);
 }
